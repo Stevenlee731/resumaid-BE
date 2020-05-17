aa25ecdaa20133f6b8ac163a3d448038a2a808db
Steven Lee <stevecreated@gmail.com>
2020-05-17 00:39:17 -0700

    add js extension

--- models/index.js
+++ models/index.js
@@ -1,39 +1,39 @@
-const UserSchema = require("./User");
+const UserSchema = require("./User.js");
 
 //modules
-const ModuleList = require("./modules/ModuleList");
-const Module = require("./modules/Module");
+const ModuleList = require("./modules/ModuleList.js");
+const Module = require("./modules/Module.js");
 
 //awards
-const AwardSchema = require("./modules/Awards/Award");
-const AwardItemSchema = require("./modules/Awards/AwardItem");
+const AwardSchema = require("./modules/Awards/Award.js");
+const AwardItemSchema = require("./modules/Awards/AwardItem.js");
 
 //basics
-const BasicSchema = require("./modules/Basics/Basic");
-const LocationSchema = require("./modules/Basics/Location");
-const SocialProfileSchema = require("./modules/Basics/SocialProfile");
+const BasicSchema = require("./modules/Basics/Basic.js");
+const LocationSchema = require("./modules/Basics/Location.js");
+const SocialProfileSchema = require("./modules/Basics/SocialProfile.js");
 
 //education
-const EducationSchema = require("./modules/Education/Education");
-const EducationItemSchema = require("./modules/Education/EducationItem");
+const EducationSchema = require("./modules/Education/Education.js");
+const EducationItemSchema = require("./modules/Education/EducationItem.js");
 
 //interests
-const InterestSchema = require("./modules/Interests/Interest");
-const InterestItemSchema = require("./modules/Interests/InterestItem");
+const InterestSchema = require("./modules/Interests/Interest.js");
+const InterestItemSchema = require("./modules/Interests/InterestItem.js");
 
 //skills
-const SkillSchema = require("./modules/Skills/Skill");
-const SkillItemSchema = require("./modules/Skills/SkillItem");
-const KeywordSchema = require("./modules/Skills/Keyword");
+const SkillSchema = require("./modules/Skills/Skill.js");
+const SkillItemSchema = require("./modules/Skills/SkillItem.js");
+const KeywordSchema = require("./modules/Skills/Keyword.js");
 
 //work
-const WorkSchema = require("./modules/Work/Work");
-const WorkItemSchema = require("./modules/Work/WorkItem");
-const HighlightSchema = require("./modules/Work/Highlight");
+const WorkSchema = require("./modules/Work/Work.js");
+const WorkItemSchema = require("./modules/Work/WorkItem.js");
+const HighlightSchema = require("./modules/Work/Highlight.js");
 
 //references
-const ReferenceSchema = require("./modules/References/Reference");
-const ReferenceItemSchema = require("./modules/References/ReferenceItem");
+const ReferenceSchema = require("./modules/References/Reference.js");
+const ReferenceItemSchema = require("./modules/References/ReferenceItem.js");
 
 module.exports = {
   ReferenceSchema,
